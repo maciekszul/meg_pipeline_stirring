556d6321568a7eadbc321d1a678c9b2567cdb07c
Maciek Szul <maciek.pwns@gmail.com>
2019-02-19 15:10:41 +0000

    ADD: readme + json input

--- 01_source_localisation.py
+++ 01_source_localisation.py
@@ -1,1 +1,6 @@
-import mne
+# import mne
+import json
+
+with open("pipeline_params.json") as pipeline_file:
+    pipeline_params = json.load(pipeline_file)
+
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # MEG pipeline: Stirring experiment
+Standard MNE-Python 0.17.1 environment
+
 
 ## order of operations:
 1. source localisation
