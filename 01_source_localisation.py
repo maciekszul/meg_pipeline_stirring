7a25619a91a8b6f1a69b68429ef1b223b1bf5ebb
Maciek Szul <maciek.pwns@gmail.com>
2019-02-19 17:43:44 +0000

    ADD: elements of pipeline added

--- 01_source_localisation.py
+++ 01_source_localisation.py
@@ -1,7 +1,8 @@
-# import mne
+import mne
 import os.path as op
 import argparse
 import json
+import numpy as np
 from utilities import files
 
 
@@ -39,6 +40,56 @@ raw_files = files.get_files(raw_dir,"", ".fif")[0]
 raw_files.sort()
 trans_file = files.get_files(trans_dir, subj, "trans.fif")[0][0]
 
+# read in the info from raw data
+info = mne.io.read_info(raw_files[0])
 
 if pipeline_params["coreg_viz"]:
-    print("Alright, alright, alright.")
+    mne.viz.plot_alignment(
+        info,
+        trans_file, 
+        subject=subj, 
+        subjects_dir=mri_dir, 
+        surfaces="head-dense", 
+        dig=True
+    )
+
+if pipeline_params["bem_surf"]:
+    import matplotlib.pylab as plt
+    mne.viz.plot_bem(
+        subject=subj,
+        subjects_dir=mri_dir,
+        brain_surfaces="white",
+        orientation="coronal"
+    )
+
+if pipeline_params["compute_source_space"]:
+    src = mne.setup_source_space(
+        subject=subj, 
+        subjects_dir=mri_dir, 
+        spacing="ico3", 
+        add_dist=False
+    )
+
+if pipeline_params["source_space_3d"]:
+    from mayavi import mlab
+    from surfer import Brain
+    hemispheres = ["lh", "rh"]
+
+    brain = Brain(
+        subj,
+        hemispheres[0],
+        "inflated",
+        subjects_dir=mri_dir
+    )
+
+    surf = brain.geo[hemispheres[0]]
+
+    vertidx = np.where(src[0]["inuse"])[0]
+
+    mlab.points3d(
+        surf.x[vertidx], 
+        surf.y[vertidx],
+        surf.z[vertidx], 
+        color=(1, 1, 0), 
+        scale_factor=1.5
+    )
