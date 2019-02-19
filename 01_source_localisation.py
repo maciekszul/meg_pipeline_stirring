4c946520749f4c6303d656da7c612e58fdc53b0f
Maciek Szul <maciek.pwns@gmail.com>
2019-02-19 17:03:37 +0000

    ADD: utilities + paths

--- 01_source_localisation.py
+++ 01_source_localisation.py
@@ -1,6 +1,44 @@
 # import mne
+import os.path as op
+import argparse
 import json
+from utilities import files
 
-with open("pipeline_params.json") as pipeline_file:
+
+# argparse input
+des = "pipeline script"
+parser = argparse.ArgumentParser(description=des)
+parser.add_argument("-f", type=str, help="JSON file with pipeline parameters")
+parser.add_argument("-n", type=int, help="id list index")
+args = parser.parse_args()
+params = vars(args)
+
+# grab the cmd args
+subj_index = params["n"]
+json_file = params["f"]
+
+# read the pipeline params
+with open(json_file) as pipeline_file:
     pipeline_params = json.load(pipeline_file)
 
+# establish paths
+data_path = pipeline_params["data_path"]
+mri_dir = op.join(data_path, "mri", "fs_results")
+subjs = files.get_folders_files(mri_dir, wp=False)[0]
+subjs.sort()
+subj = subjs[:-1][subj_index]
+
+mri_path = op.join(mri_dir, subj)
+out_path = op.join(data_path, "stirring_source", subj)
+files.make_folder(out_path)
+raw_dir = op.join(data_path, "raw", subj)
+trans_dir = op.join(data_path, "dig", subj)
+
+# paths to files
+raw_files = files.get_files(raw_dir,"", ".fif")[0]
+raw_files.sort()
+trans_file = files.get_files(trans_dir, subj, "trans.fif")[0][0]
+
+
+if pipeline_params["coreg_viz"]:
+    print("Alright, alright, alright.")
---
+++ utilities/files.py
@@ -0,0 +1,51 @@
+import os
+
+def get_folders_files(path, wp=True):
+    """
+    Returns lists of files and folders from directory
+    path - folder address
+    wp - BOOL, list with path
+    """
+    files = []
+    folders = []
+    for (dirpath, dirnames, filenames) in os.walk(path):
+        files.extend(filenames)
+        folders.extend(dirnames)
+        break
+    if wp:
+        files = [path + os.sep + i for i in files]
+        folders = [path + os.sep + i for i in folders]
+
+    return (folders, files)
+
+
+def get_files(path, prefix, extension, wp=True):
+    """
+    Returns lists of files within folder with specific
+    prefix, extension and both.
+    path - folder address
+    wp - BOOL, list with path
+    prefix - string, beginning of the filename
+    extension - string, extension w/o dot
+    """
+    ext = []
+    prfx = []
+    intersect = []
+    files = get_folders_files(path, wp=False)
+    for i in files[1]:
+        if i.startswith(prefix):
+            prfx.append(i)
+        if i.endswith(extension):
+            ext.append(i)
+        if i.endswith(extension) and i.startswith(prefix):
+            intersect.append(i)
+    if wp:
+        ext = [path + os.sep + i for i in ext]
+        prfx = [path + os.sep + i for i in prfx]
+        intersect = [path + os.sep + i for i in intersect]
+
+    return (ext, prfx, intersect)
+
+def make_folder(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
